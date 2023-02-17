00a4d30857d243649818ca1ad8aae2fbe76de08d
Aroxfaith <arowojolufaith@gmail.com>
2023-02-16 18:43:06 -0800

    advanced

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,12 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float e;
+	float f;
 
 	printf("Size of a char: %lu btye(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a int: %lu btye(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu btye(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu btye(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu btye(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a float: %lu btye(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
