badd66f60fbc8f9e02b06896a41e7a8f6710ec3a
Aroxfaith <arowojolufaith@gmail.com>
2023-02-22 18:34:13 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,6 +2,7 @@
 
 /**
  * print_to_98 - Prints natural numbers from 98.
+ *
  * @n: Number to start from.
  */
 void print_to_98(int n)
@@ -9,14 +10,14 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-			_putchar("%d, ", n--);
-		_putchar("%d\n", n);
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
 
 	else
 	{
 		while (n < 98)
-			_putchar("%d, ", n++);
-		_putchar("%d\n", n);
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
